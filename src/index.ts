3185e33f0856c5973c607e864ba1374cc8d6c003
Rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2023-01-02 09:20:09 -0300

    Adição da porta padrão express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Deu bom");
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+
+app.get("/status", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ foo: "sucesso total" })
+})
+app.listen(3000, () => {
+    console.log("Apicação executando na porta 3000")
+})
